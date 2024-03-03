0d79eb5c17778d2b504c094d5b458ecade752194
Martin Bradbury <martinbradbury@btinternet.com>
2024-03-03 19:34:27 +0000

    added teh about app and linked it with model view and admin

--- about/models.py
+++ about/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class About(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    updated_on = models.DateTimeField(auto_now=True)
+
+    #this lets us see it is working
+    def __str__(self):
+        return self.title
+

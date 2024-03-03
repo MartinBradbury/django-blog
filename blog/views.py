29b1143fb77c485b4ec35a42e2c4b12792b3006a
Martin Bradbury <martinbradbury@btinternet.com>
2024-03-03 22:47:56 +0000

    added views to see comments on post

--- blog/views.py
+++ blog/views.py
@@ -34,9 +34,15 @@ def post_detail(request, slug):
 
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.all()
+    comment_count = post.comments.filter(approved=True).count()
 
     return render(
         request,
         "blog/post_detail.html",
-        {"post": post, "coder": "Martin Bradbury"},
-    )
+        {
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+        },
+)
